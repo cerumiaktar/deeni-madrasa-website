90d6f1bdf18b0ee81cba64a8a889fa3876bb953a
unknown <ce.rumiakter@gmail.com>
2025-08-09 18:26:29 +0600

    add countries section and flag row 1

--- src/pages/Home/Countries/Countries.jsx
+++ src/pages/Home/Countries/Countries.jsx
@@ -4,9 +4,126 @@ const Countries = () => {
     return (
         <div className="bg-[#14535FB3] py-20 mt-20">
             <div className="container mx-auto">
-                <div className="text-center">
+                <div className="text-center mb-6">
                     <h1 className="text-2xl font-bold">WHY DEENI MADRASA</h1>
                     <p>Learn Quran and many other subjects of Islamic studies with live classes at with great features and benefits.</p>
+                </div>
+                <div >
+                    {/* col 1 */}
+                    <div className="grid grid-cols-2 md:grid-cols-5 items-center">
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/tPs8ddd6/openmoji-flag-australia.png" alt="" />
+                            <h3>Australia</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/DH1s5vp7/openmoji-flag-united-states.png" alt="" />
+                            <h3>United States</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/5x5g5hf1/openmoji-flag-canada.png" alt="" />
+                            <h3>Canada</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/JW328ScM/openmoji-flag-united-kingdom.png" alt="" />
+                            <h3>United Kingdom</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/0HdFrZK/openmoji-flag-new-zealand.png" alt="" />
+                            <h3>New Zealand</h3>
+                        </div>
+                    </div>
+                    {/* col 2 */}
+                    <div className="grid grid-cols-2 md:grid-cols-5 items-center">
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/RGw3L4rj/openmoji-flag-afghanistan.png" alt="" />
+                            <h3>Afghanistan</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/rRxX3yZS/openmoji-flag-denmark.png" alt="" />
+                            <h3>Denmark</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/x8HJSQSM/openmoji-flag-india.png" alt="" />
+                            <h3>India</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/jP9Kdsbr/openmoji-flag-norway.png" alt="" />
+                            <h3>Norway</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/nNbjCYFy/openmoji-flag-saudi-arabia.png" alt="" />
+                            <h3>Saudi Arabia</h3>
+                        </div>
+                    </div>
+                    {/* col 3 */}
+                    <div className="grid grid-cols-2 md:grid-cols-5 items-center">
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/tPs8ddd6/openmoji-flag-australia.png" alt="" />
+                            <h3>Australia</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/DH1s5vp7/openmoji-flag-united-states.png" alt="" />
+                            <h3>United States</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/5x5g5hf1/openmoji-flag-canada.png" alt="" />
+                            <h3>Canada</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/JW328ScM/openmoji-flag-united-kingdom.png" alt="" />
+                            <h3>United Kingdom</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/tPs8ddd6/openmoji-flag-australia.png" alt="" />
+                            <h3>New Zealand</h3>
+                        </div>
+                    </div>
+                    {/* col 4 */}
+                    <div className="grid grid-cols-2 md:grid-cols-5 items-center">
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/tPs8ddd6/openmoji-flag-australia.png" alt="" />
+                            <h3>Australia</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/DH1s5vp7/openmoji-flag-united-states.png" alt="" />
+                            <h3>United States</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/5x5g5hf1/openmoji-flag-canada.png" alt="" />
+                            <h3>Canada</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/JW328ScM/openmoji-flag-united-kingdom.png" alt="" />
+                            <h3>United Kingdom</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/tPs8ddd6/openmoji-flag-australia.png" alt="" />
+                            <h3>New Zealand</h3>
+                        </div>
+                    </div>
+                    {/* col 5 */}
+                    <div className="grid grid-cols-2 md:grid-cols-5 items-center">
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/tPs8ddd6/openmoji-flag-australia.png" alt="" />
+                            <h3>Australia</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/DH1s5vp7/openmoji-flag-united-states.png" alt="" />
+                            <h3>United States</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/5x5g5hf1/openmoji-flag-canada.png" alt="" />
+                            <h3>Canada</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/JW328ScM/openmoji-flag-united-kingdom.png" alt="" />
+                            <h3>United Kingdom</h3>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <img src="https://i.ibb.co.com/tPs8ddd6/openmoji-flag-australia.png" alt="" />
+                            <h3>New Zealand</h3>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
