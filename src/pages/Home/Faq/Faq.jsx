7d11b86ecf048ae7b76d7dd05bcd63360a4dcac3
unknown <ce.rumiakter@gmail.com>
2025-08-11 12:59:16 +0600

    update FAQ for dark mode

--- src/pages/Home/Faq/Faq.jsx
+++ src/pages/Home/Faq/Faq.jsx
@@ -7,30 +7,30 @@ const Faq = () => {
                 <h1 className='text-2xl font-bold text-black'>FAQ</h1>
             </div>
             <div className="space-y-4">
-                <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+                <div className="collapse collapse-arrow bg-base-100 border border-base-300 dark:bg-white dark:border dark:border-gray-300">
                     <input type="radio" name="my-accordion-2" defaultChecked />
-                    <div className="collapse-title font-semibold">What is a Madrasa?</div>
-                    <div className="collapse-content text-sm">A Madrasa is an educational institution focused primarily on teaching Islamic religious studies, including the Quran, Hadith, Fiqh (Islamic jurisprudence), and Arabic language.</div>
+                    <div className="collapse-title font-semibold dark:text-black">What is a Madrasa?</div>
+                    <div className="collapse-content dark:text-[#333333] text-sm">A Madrasa is an educational institution focused primarily on teaching Islamic religious studies, including the Quran, Hadith, Fiqh (Islamic jurisprudence), and Arabic language.</div>
                 </div>
-                <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+                <div className="collapse collapse-arrow bg-base-100 border border-base-300 dark:bg-white dark:border dark:border-gray-300">
                     <input type="radio" name="my-accordion-2" />
-                    <div className="collapse-title font-semibold">What subjects are taught in a Madrasa?</div>
-                    <div className="collapse-content text-sm">Besides religious studies like Quran, Hadith, and Islamic law, many Madrasas also teach Arabic, Islamic history, and sometimes general subjects such as math, science, and languages depending on the institution.</div>
+                    <div className="collapse-title font-semibold dark:text-black">What subjects are taught in a Madrasa?</div>
+                    <div className="collapse-content dark:text-[#333333] text-sm">Besides religious studies like Quran, Hadith, and Islamic law, many Madrasas also teach Arabic, Islamic history, and sometimes general subjects such as math, science, and languages depending on the institution.</div>
                 </div>
-                <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+                <div className="collapse collapse-arrow bg-base-100 border border-base-300 dark:bg-white dark:border dark:border-gray-300">
                     <input type="radio" name="my-accordion-2" />
-                    <div className="collapse-title font-semibold">Who can attend a Madrasa?</div>
-                    <div className="collapse-content text-sm">Generally, Madrasas are open to Muslim students of all ages who want to gain knowledge about Islam. Some Madrasas are for children, while others offer advanced studies for adults and scholars.</div>
+                    <div className="collapse-title font-semibold dark:text-black">Who can attend a Madrasa?</div>
+                    <div className="collapse-content text-sm dark:text-[#333333]">Generally, Madrasas are open to Muslim students of all ages who want to gain knowledge about Islam. Some Madrasas are for children, while others offer advanced studies for adults and scholars.</div>
                 </div>
-                <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+                <div className="collapse collapse-arrow bg-base-100 border border-base-300 dark:bg-white dark:border dark:border-gray-300">
                     <input type="radio" name="my-accordion-2" />
-                    <div className="collapse-title font-semibold">Is Madrasa education recognized by governments or universities?</div>
-                    <div className="collapse-content text-sm">Recognition varies by country. In some places, Madrasa education is integrated into the formal education system, while in others it remains separate. Some universities accept Madrasa qualifications for higher education in Islamic studies.</div>
+                    <div className="collapse-title font-semibold dark:text-black">Is Madrasa education recognized by governments or universities?</div>
+                    <div className="collapse-content text-sm dark:text-[#333333]">Recognition varies by country. In some places, Madrasa education is integrated into the formal education system, while in others it remains separate. Some universities accept Madrasa qualifications for higher education in Islamic studies.</div>
                 </div>
-                <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+                <div className="collapse collapse-arrow bg-base-100 border border-base-300 dark:bg-white dark:border dark:border-gray-300">
                     <input type="radio" name="my-accordion-2" />
-                    <div className="collapse-title font-semibold">Do Madrasas only focus on religious education?</div>
-                    <div className="collapse-content text-sm">Traditionally, Madrasas focus on religious education, but many modern Madrasas have incorporated secular subjects to provide students with a balanced education.</div>
+                    <div className="collapse-title font-semibold dark:text-black">Do Madrasas only focus on religious education?</div>
+                    <div className="collapse-content text-sm dark:text-[#333333]">Traditionally, Madrasas focus on religious education, but many modern Madrasas have incorporated secular subjects to provide students with a balanced education.</div>
                 </div>
             </div>
         </div>
